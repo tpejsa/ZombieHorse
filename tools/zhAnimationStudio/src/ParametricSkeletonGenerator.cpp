c1b5ad410014fd06d5415f48615c79033faf98bd
cs.lhxac <cs.lhxac@gmail.com>
2013-03-15 15:06:28 -0500

    Gnerator modified

--- tools/zhAnimationStudio/src/ParametricSkeletonGenerator.cpp
+++ tools/zhAnimationStudio/src/ParametricSkeletonGenerator.cpp
@@ -3,26 +3,19 @@
 #include "ParametricSkeletonGenerator.h"
 #include "cstring"
 #include "boost\regex.hpp"
-BonePairClass ClassifyBonePair(zh::Bone* parent,zh::Bone* child){
-
-	return BODY;
-}
-bool ParametricSkeletonGenerate(zh::Bone* currentOut,zh::Bone* currentTemplate,zh::Skeleton* skelOut,SkeletonParameters parameters){
-	for(zh::Bone::ChildIterator i = currentTemplate->getChildIterator();!i.end();){
+bool ParametricSkeletonScale(zh::Bone* currentOut,zh::Bone* currentTemplate,SkeletonParameters parameters){
+	for(zh::Bone::ChildIterator i = currentTemplate->getChildIterator(),
+		j = currentOut->getChildIterator();!i.end();){
 		zh::Bone* childTemplate = i.next();
-		zh::Bone* childOut = skelOut -> createBone(childTemplate -> getId(),childTemplate -> getName());
-		double scale = parameters.scaleGlobal * parameters[ClassifyBonePair(currentTemplate,childTemplate)];
+		zh::Bone* childOut = j.next();
+		double scale = parameters.scaleGlobal * parameters[currentTemplate/*tagHere!!!*/];
 		childOut -> setInitialPosition(childTemplate ->getInitialPosition() * scale);
-		childOut -> setInitialOrientation(childTemplate ->getInitialOrientation());
-		currentOut -> addChild(childOut);
-		ParametricSkeletonGenerate(childOut,childTemplate,skelOut,parameters);
+		ParametricSkeletonScale(childOut,childTemplate,parameters);
 	}
 	return true;
 }
 bool ParametricSkeletonGenerator(zh::Skeleton* skelOut,zh::Skeleton* templateSkel,SkeletonParameters parameters){
 	zh::Bone* rootTemplate = templateSkel -> getRoot();
-	zh::Bone* rootOut = skelOut->createBone(rootTemplate->getId(),rootTemplate->getName());
-	rootOut ->setInitialOrientation(rootTemplate->getInitialOrientation());
-	rootOut ->setInitialPosition(rootTemplate->getInitialPosition());
-	return ParametricSkeletonGenerate(rootOut,rootTemplate,skelOut,parameters);
+	zh::Bone* rootOut = skelOut->getRoot();
+	return ParametricSkeletonScale(rootOut,rootTemplate,parameters);
 }
