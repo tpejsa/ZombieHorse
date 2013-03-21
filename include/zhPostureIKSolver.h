7bea549a96420a1efdf6fd3b15150871c4568360
cs.lhxac <cs.lhxac@gmail.com>
2013-03-21 18:36:16 -0500

    Numerical Working!!!!!!!

--- include/zhPostureIKSolver.h
+++ include/zhPostureIKSolver.h
@@ -25,39 +25,101 @@ SOFTWARE.
 
 #include "zhPrereq.h"
 #include "zhIKSolver.h"
-
+#include <vector>
 namespace zh
 {
-
-/**
-* @brief Class representing an IK solver for positioning the figure root.
-*/
-class zhDeclSpec PostureIKSolver : public IKSolver
-{
-
-public:
-
-	zhDeclare_IKSolver( PostureIKSolver, zhPostureIKSolver_ClassId, zhPostureIKSolver_ClassName )
-
 	/**
-	* Constructor.
+	* @brief Class representing an IK solver for positioning the figure root.
 	*/
-	PostureIKSolver();
+	class zhDeclSpec PostureIKSolver : public IKSolver
+	{
 
-	/**
-	* Destructor.
-	*/
-	~PostureIKSolver();
+	public:
 
-	/**
-	* Compute the skeletal pose that meets the current IK goals.
-	*/
-	void solve();
+		zhDeclare_IKSolver( PostureIKSolver, zhPostureIKSolver_ClassId, zhPostureIKSolver_ClassName )
 
-protected:
+		/**
+		* Constructor.
+		*/
+		PostureIKSolver();
 
-};
+		/**
+		* Destructor.
+		*/
+		~PostureIKSolver();
 
+		/**
+		* Compute the skeletal pose that meets the current IK goals.
+		*/
+		void solve();
+
+	protected:
+		struct VectorXD{//high dimensional vector
+			int dimension;
+			vector<double> values;
+			void add(VectorXD v2){
+				if(dimension == v2.dimension){
+					for(int i = 0;i < dimension;++i){
+						values[i] += v2.values[i];
+					}
+				}
+			}
+			double& operator[](int index){
+				return values[index];
+			}
+			void setDimension(int d){
+				dimension = d;
+				values.resize(d);
+			}
+			void subtract(VectorXD v2){
+				if(dimension == v2.dimension){
+					for(int i = 0;i < dimension;++i){
+						values[i] -= v2.values[i];
+					}
+				}
+			}
+			VectorXD& revert(){//revert the value,  used to find the negative gredient
+				for(int i = 0;i < dimension;++i){
+					values[i] = -values[i];
+				}
+				return *this;
+			}
+			VectorXD& operator*(double c){
+				for(int i = 0;i < dimension;++i){
+					values[i] *= c;
+				}
+				return *this;
+			}
+			double length(){
+				double length = 0;
+				for(int i = 0;i < dimension;++i){
+					length += values[i]*values[i];
+				}
+				return sqrt(length);
+			}
+		};
+		int countGoal;
+		int countBone;
+		bool initSolve;
+		VectorXD gredient;
+		void CGIteration();//The conjegate gredient iteration
+		double initStepSize;
+		double gredientThreshold;//defines the threshold to stop iteration
+		int maxIteration;
+		double lineSearchStepSize;
+		double lineSearch(int index, VectorXD& direction);//search argmin along that direction, the index is the index of the snapshot of the start point
+		//the step is applied after the line search
+		void applyConfiguration(VectorXD& config);//apply the current configuration to the skeleton
+		void GD();//The gredient decent
+		double energy()const;//Masures the energy function of te current configuration
+		void computeGredient();
+		void snapshot(int index);//take a snapshot of current joint configuration save it in shot[index];
+		void restoreSnapshot(int index);
+		struct Snapshot{
+			Vector3 rootPosition;
+			vector<Quat> rotation;
+		};
+		Snapshot shot[2];
+	};
 }
-
 #endif // __zhPostureIKSolver_h__
--- src/zhPostureIKSolver.cpp
+++ src/zhPostureIKSolver.cpp
@@ -21,20 +21,208 @@ SOFTWARE.
 ******************************************************************************/
 
 #include "zhPostureIKSolver.h"
-
+#include"zhBone.h"
+#include"zhSkeleton.h"
+#include <Eigen/Eigen>
 namespace zh
 {
 
-PostureIKSolver::PostureIKSolver()
-{
-}
+	PostureIKSolver::PostureIKSolver()
+	{
+		initSolve = true;
+		initStepSize = 10;
+		gredientThreshold = 100;//defines the threshold to stop iteration
+		maxIteration = 30;
+		countGoal = 0;
+		countBone = 0;
+	}
 
-PostureIKSolver::~PostureIKSolver()
-{
-}
+	PostureIKSolver::~PostureIKSolver()
+	{
+	}
+	void PostureIKSolver::solve()
+	{
+		lineSearchStepSize = initStepSize;
+		if(initSolve){
+			initSolve = false;
+			GoalConstIterator gi = getGoalConstIterator();
+			while(!gi.end()){
+				gi.next();
+				countGoal++;
+			}
+			zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+			while(!bi.end()){
+				bi.next();
+				countBone++;
+			}
+			gredient.setDimension(countBone * 3 + 3);
+		}
+		snapshot(0);
+		/*zh::Skeleton::BoneIterator bi = mSkel->getBoneIterator();
+		if(!bi.end()){
+			Bone* bone = bi.next();
+			//bone ->setPosition(zh::Vector3(100,0,0));
+			//bone->rotate(Quat(3.14/4,3.14/4,3.14/4,3.14/4).normalize());
+			Quat tmp(0,0,0,3.14/4);
+			tmp = tmp.exp();
+			tmp = tmp.normalize();
+			bone ->setOrientation(bone->getOrientation()*tmp);
+		}*/
+		//restoreSnapshot(1);
+		/*while(!bi.end()){
+			Bone* bone = bi.next();
+			Quat tmp(0,0,0,3.14/4);
+			bone ->setOrientation(bone->getOrientation()*tmp.exp());
+		}*/
+		//restoreSnapshot(1);
+		GD();
+		zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+		int counter = 0;
+		if(!bi.end()){
+			Bone* bone = bi.next();
+			if(!this ->findBone(bone->getId())){
+				bone ->setPosition(shot[0].rootPosition);
+				bone ->setOrientation(shot[0].rotation[counter]);
+			}
+			counter++;
+		}
+		while(!bi.end()){
+			Bone* bone = bi.next();
+			if(!this ->findBone(bone->getId())){
+				bone ->setOrientation(shot[0].rotation[counter]);
+			}
+			counter++;
+		}
+	}
+	void PostureIKSolver::restoreSnapshot(int index){
+		zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+		int counter = 0;
+		if(!bi.end()){
+			Bone* bone = bi.next();
+			bone ->setPosition(shot[index].rootPosition);
+			bone ->setOrientation(shot[index].rotation[counter++]);
+		}
+		while(!bi.end()){
+			Bone* bone = bi.next();
+			bone ->setOrientation(shot[index].rotation[counter++]);
+		}	
+	}
+	void PostureIKSolver::snapshot(int index){
+		shot[index].rotation.clear();
+		zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+		if(!bi.end()){
+			Bone* bone = bi.next();
+			shot[index].rootPosition = bone->getPosition();
+			shot[index].rotation.push_back(bone -> getOrientation());
+		}
+		while(!bi.end()){
+			Bone* bone = bi.next();
+			shot[index].rotation.push_back(bone -> getOrientation());
+		}
+	}
+	void PostureIKSolver::CGIteration(){
+		
+	}
+	void PostureIKSolver::applyConfiguration(VectorXD& config){
+		zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+		int counter = 2;
+		if(!bi.end()){
+				Bone* bone = bi.next();
+				bone -> setPosition(bone ->getPosition() + Vector3(config.values[0],config.values[1],config.values[2]));
+				bone -> setOrientation(bone -> getOrientation() * Quat(0,config.values[3],config.values[4],config.values[5]).exp());
+		}
+		while(!bi.end()){
+				Bone* bone = bi.next();
+				bone -> setOrientation(bone -> getOrientation() * Quat(0,config.values[counter * 3],config.values[counter * 3 + 1],config.values[counter * 3 + 2]).exp());
+				counter++;
+		}
+	}
+	double PostureIKSolver::lineSearch(int index, VectorXD& direction){
+		double energyO = energy();
+		direction * lineSearchStepSize;
+		do{
+			//try step
+			applyConfiguration(direction);
+			double tmp = energy();
+			if(tmp < energyO){
+				return lineSearchStepSize;
+			}
+			restoreSnapshot(index);
+			lineSearchStepSize /= 2;
+			direction * 0.5;
+		}while(1);
+	}
+	void PostureIKSolver::GD(){
+		for(int i = 0;i < maxIteration;++i){
+			computeGredient();
+			double tmp = gredient.length();
+			if(tmp > gredientThreshold){
+				snapshot(1);
+				lineSearch(1, gredient.revert());
+			}else{
+				break;
+			}
+		}
+	}
+	double PostureIKSolver::energy()const{
+		//currently this only have the first part of the equation(distance matric)
+		GoalConstIterator gi = getGoalConstIterator();
+		double energy = 0;
+		while(!gi.end()){
+			IKGoal goal = gi.next();
+			Vector3 tmp = mSkel->getBone(goal.boneId)->getWorldPosition();
+			energy += goal.weight*goal.position.distanceSq(mSkel->getBone(goal.boneId)->getWorldPosition());
+		}
+		return energy;
+	}
+	void PostureIKSolver::computeGredient(){
+		double step = 0.0001;
+		double energyO = energy();
+		
+		gredient[0] = 0;
+		gredient[1] = 0;
+		gredient[2] = 0;
+		
+		zh::Skeleton::BoneIterator bi = mSkel -> getBoneIterator();
+		int boneCounter = 0;
+		//gredient for root position
+		/*GoalConstIterator gi = getGoalConstIterator();
+		while(!gi.end()){
+			IKGoal goal = gi.next();
+			gredient[0] += 2 * (mSkel->getBone(goal.boneId)->getWorldPosition() - goal.position).x;
+			gredient[1] += 2 * (mSkel->getBone(goal.boneId)->getWorldPosition() - goal.position).y;
+			gredient[2] += 2 * (mSkel->getBone(goal.boneId)->getWorldPosition() - goal.position).z;
+		}*/
 
-void PostureIKSolver::solve()
-{
-}
+		bi = mSkel -> getBoneIterator();
+		Bone* bone = bi.next();
+		Vector3 snap = bone ->getPosition();
+		bone -> setPosition(snap + Vector3(step,0,0));
+		gredient[0] = (energy() - energyO) / step;
+		
+		bone -> setPosition(snap + Vector3(0,step,0));
+		gredient[1] = (energy() - energyO) / step;
+		
+		bone -> setPosition(snap + Vector3(0,0,step));
+		gredient[2] = (energy() - energyO) / step;
+		bi = mSkel -> getBoneIterator();
+		bone -> setPosition(snap);
 
+		while(!bi.end()){
+				Bone* bone = bi.next();
+				Quat snap = bone -> getOrientation();
+				//x rotation
+				bone -> setOrientation(snap*(Quat(0,step,0,0).exp()));
+				gredient[boneCounter * 3 + 3] = (energy() - energyO) / step;
+				//y rotation
+				bone -> setOrientation(snap*(Quat(0,0,step,0).exp()));
+				gredient[boneCounter * 3 + 4] = (energy() - energyO) / step;
+				//z rotation
+				bone -> setOrientation(snap*(Quat(0,0,0,step).exp()));
+				gredient[boneCounter * 3 + 5] += (energy() - energyO) / step;
+				//reset orientation
+				bone -> setOrientation(snap);
+				boneCounter++;
+		}
+	}
 }
